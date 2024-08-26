0245e9dd8b0db0fbb7d323206754c904b8fa0e9d
santiago gonzalez lojo <santiagolojo2002@gmail.com>
2024-08-26 11:12:23 -0300

    add new logic

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
-const express = require('express');
+const express = require("express");
+require("dotenv").config();
+const { NoLlame, Op } = require("./sql");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/search/:number", (req, res) => {
+  const number = req.params["number"];
+  NoLlame.findAll({
+    where: {
+      numero_s: {
+        [Op.like]: `%${number}%`, // Para números que terminan con '092095737'
+      },
+    },
+  })
+    .then((v) => {
+      return res.json(v.map((e) => e.dataValues));
+    })
+    .catch((err) => res.json({ err }));
 });
 
-app.listen(port, () => {
-  console.log(`Servidor ejecutándose en http://localhost:${port}`);
+app.get("/exist/:number", (req, res) => {
+  const number = req.params["number"];
+  NoLlame.findOne({
+    where: {
+      numero_s: {
+        [Op.like]: `%${number}%`, // Para números que terminan con '092095737'
+      },
+    },
+  })
+    .then((v) => {
+      const exists = !!v;
+      return res.json({ exists });
+    })
+    .catch((err) => res.json({ err }));
 });
+
+app.get("/add/:number", (req, res) => {
+  const number = req.params["number"];
+  NoLlame.create({
+    numero_s: number,
+  });
+});
+
+app.get("/", (req, res) => {
+  NoLlame.findAll()
+    .then((v) => {
+      return res.json(v.map((e) => e.dataValues));
+    })
+    .catch((err) => res.json({ err }));
+});
+
+async function init() {
+  console.log(`Starting Sequelize + Express example on port ${port}...`);
+
+  app.listen(port, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${port}`);
+  });
+}
+
+init();
 
 module.exports = app;
---
+++ sql.js
@@ -0,0 +1,31 @@
+const { Sequelize, DataTypes,Op } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_TABLE,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALET,
+  }
+);
+sequelize.authenticate();
+console.log("Database connection OK!");
+
+const NoLlame = sequelize.define(
+  "no_llame",
+  {
+    numero_s: {
+      type: DataTypes.STRING, // Especificar que es un VARCHAR
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "no_llame", // Nombre de la tabla
+  }
+);
+
+sequelize.sync(); // Este método crea las tablas si no existen
+
+module.exports = { sequelize, NoLlame,Op };
